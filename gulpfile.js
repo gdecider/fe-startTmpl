9671976da37752cf6522e04cba295b78ca1634f9
Denis.M <decider@ya.ru>
2017-05-14 22:34:09 +0300

    Изменена структура

--- gulpfile.js
+++ gulpfile.js
@@ -14,42 +14,45 @@ var cache       = require('gulp-cache');
 var autoprefixer= require('gulp-autoprefixer');
 var sourcemaps  = require('gulp-sourcemaps');
 
+var srcPath = './app';
+var distPath = './dist';
+
 // обработка sass
 gulp.task('sass', function(){
-	return gulp.src('./app/sass/**/*.+(sass|scss)')
+	return gulp.src(srcPath + '/sass/**/*.+(sass|scss)')
 			.pipe(sass())
 			.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
-			.pipe(gulp.dest('app/css'))
+			.pipe(gulp.dest(srcPath + '/css'))
 			.pipe(browserSync.reload({stream: true}));
 });
 
 // uglify
 gulp.task('scripts', function(){
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',		
+		srcPath + '/vendors/jquery/dist/jquery.min.js',		
 	])
-	.pipe(concat('libs.min.js'))
+	.pipe(concat('vendors.min.js'))
 	.pipe(uglify())
-	.pipe(gulp.dest('app/js'));
+	.pipe(gulp.dest(srcPath + '/js'));
 });
 
 // uglify css libs
 gulp.task('minify-css', ['sass'], function(){
 	return gulp.src([
-		'app/css/**/*.css',		
+		srcPath + '/css/**/*.css',		
 	])
 	.pipe(sourcemaps.init())
 	.pipe(cleanCss())
 	.pipe(sourcemaps.write())
 	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('app/css'));
+	.pipe(gulp.dest(srcPath + '/css'));
 });
 
 // browser-sync
 gulp.task('sync', function(){
 	browserSync({
 		server: {
-			baseDir: 'app',
+			baseDir: srcPath,
 		},
 		notify: false
 	});
@@ -57,19 +60,19 @@ gulp.task('sync', function(){
 
 // minify img
 gulp.task('img', function(){
-	return gulp.src('app/img/**/*')
+	return gulp.src(srcPath + '/img/**/*')
 	.pipe(cache(imagemin({
 		interlaced: true,
 		progressive: true,
 		svgoPluggins: [{removeViewBox: false}],
 		use: [pngquant()]
 	})))
-	.pipe(gulp.dest('dist/img'));
+	.pipe(gulp.dest(distPath + '/img'));
 });
 
 // clean build dir
 gulp.task('clean', function(){
-	return del.sync('dist');
+	return del.sync(distPath);
 });
 
 // clear cache
@@ -79,31 +82,30 @@ gulp.task('clear', function(){
 
 // watcher
 gulp.task('watch', ['sync', 'minify-css', 'scripts'], function(){
-	gulp.watch('./app/sass/**/*.+(sass|scss)', ['sass']);
-	gulp.watch('app/*.html', browserSync.reload);
-	gulp.watch('app/js/**/*.js', browserSync.reload);
+	gulp.watch(srcPath + '/sass/**/*.+(sass|scss)', ['sass']);
+	gulp.watch(srcPath + '/**/*.html', browserSync.reload);
+	gulp.watch(srcPath + '/js/**/*.js', browserSync.reload);
 });
 
 // build
 gulp.task('build', ['clean', 'minify-css', 'scripts', 'img'], function(){
 	var buildCss = gulp.src([
-		'app/css/main.css',
-		'app/css/libs.min.css',
+		srcPath + '/css/**/*.css',
 	])
-	.pipe(gulp.dest('dist/css'));
+	.pipe(gulp.dest(distPath + '/css'));
 
 	var buildFonts = gulp.src([
-		'app/fonts/**/*',
+		srcPath + '/fonts/**/*',
 	])
-	.pipe(gulp.dest('dist/fonts'));
+	.pipe(gulp.dest(distPath + '/fonts'));
 
 	var buildJs = gulp.src([
-		'app/js/**/*',
+		srcPath + '/js/**/*',
 	])
-	.pipe(gulp.dest('dist/js'));
+	.pipe(gulp.dest(distPath + '/js'));
 
 	var buildHtml = gulp.src([
-		'app/*.html',
+		srcPath + '/**/*.html',
 	])
-	.pipe(gulp.dest('dist'));
+	.pipe(gulp.dest(distPath));
 });
